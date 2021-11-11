d16e704b05c37d0c8e241167175b99c6d89962ce
steve weber <s8weber@uwaterloo.ca>
2021-11-11 12:05:47 -0500

    some python3 fixes

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='libinfoblox',
-    version='1.0',
-    description='Access infoblox API',
-    author='Steve Weber',
-    author_email='steverweber@gmail.com',
-    url = 'https://github.com/steverweber/libinfoblox',
-    download_url = 'https://github.com/steverweber/libinfoblox/archive/1.0.tar.gz',
+    name="libinfoblox",
+    version="1.0",
+    description="Access infoblox API",
+    author="Steve Weber",
+    author_email="steverweber@gmail.com",
+    url="https://github.com/steverweber/libinfoblox",
+    download_url="https://github.com/steverweber/libinfoblox/archive/1.0.tar.gz",
     packages=find_packages(),
-    install_requires=['requests', 'netaddr'],
+    install_requires=["requests", "netaddr"],
 )
